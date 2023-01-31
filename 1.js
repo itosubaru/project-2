55d1a5837a605059b8ddfa16c910f620ecffc1eb
itosubaru <103973953+itosubaru@users.noreply.github.com>
2023-01-31 21:13:19 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,5 @@
-function mondai1(){if(document.getElementById('kotae1').value=='徳川家康'){
+function mondai1(){
+  if(document.getElementById('kotae1').value=='徳川家康'){
   Swal.fire('正解！' , '<a href="1.html" target"_blank">解説へ</a>');}
         else{alert('はずれ！');}
                   }
