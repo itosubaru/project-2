6ae19469b395bacecd4adbe2f4bb8dd7f65dfcb4
itosubaru <103973953+itosubaru@users.noreply.github.com>
2023-02-06 17:53:57 +0900

    Update EX.js

--- EX.js
+++ EX.js
@@ -1,4 +1,4 @@
-function modnaiEX(){if(document.getElementById('kotaeEX').value=='天海'){
+function mondaiEX(){if(document.getElementById('kotaeEX').value=='天海'){
   alert('正解！解説へ飛ぶよ。');
   window.open("EX.html");}
   else {alert('はずれ');}
