164ab2f9d38dc7a921a35db54e1431e8881eeaca
itosubaru <103973953+itosubaru@users.noreply.github.com>
2023-02-06 16:49:22 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,4 @@
-function mondai1(){
-  if(document.getElementById('kotae1').value=='徳川家康'){
-  ('正解！');}
-        else{alert('はずれ！');}
+function mondai1(){if(document.getElementById('kotae1').value=='徳川家康'){
+  alert('正解');
+  else {alert('はずれ');}
 }
